8ac929e6be7c3ff594c48d6aba41e7f345bbdaf7
Denis John <denis.john.ndarake@gmail.com>
2020-02-07 13:21:38 +0100

    Completed reset password

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ use Illuminate\Http\Request;
 
 Route::post('/register', 'Api\AuthController@register');
 Route::post('/login', 'Api\AuthController@login');
+Route::post('/password/email', 'Api\ForgotPasswordController@sendResetLinkEmail');
+Route::post('/password/reset', 'Api\ResetPasswordController@reset');
